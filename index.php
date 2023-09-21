b9ce7d87cc8e0bba120e53cd8f8ae5848e486bbf
FredericNguyen <fredericnguyen@yahoo.com>
2023-09-21 14:20:48 -0400

    added login modals in index

--- index.php
+++ index.php
@@ -75,6 +75,92 @@
                             </div>
                         </div>
                 </div>
+                <!-- Modal pour enregistrer membre -->
+            <div class="modal fade" id="enregModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Enregistrer membre</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                        <form id="formEnreg" action="serveur/membre/controleurMembre.php" method="POST" enctype="multipart/form-data" class="row g-3" onSubmit="return validerFormEnreg();">
+                            <div class="col-md-6">
+                                <label for="prenom" class="form-label">Prénom</label>
+                                <input type="text" class="form-control is-valid" id="prenom" name="prenom" required>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="nom" class="form-label">Nom</label>
+                                <input type="text" class="form-control is-valid" id="nom" name="nom" required>
+                            </div>
+                            <div class="col-md-12">
+                                <label for="courriel" class="form-label">Courriel</label>
+                                <input type="email" class="form-control is-valid" id="courriel" name="courriel" required>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="pass" class="form-label">Mot de passe</label>
+                                <input type="password" class="form-control is-valid" pattern="^[A-Za-z0-9_\$#\-]{6,10}$" id="mdp" name="mdp" required>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="cpass" class="form-label">Confirmer mot de passe</label>
+                                <input type="password" class="form-control is-valid" pattern="^[A-Za-z0-9_\$#\-]{6,10}$" id="mdpc" name="mdpc" required>
+                                <span id="msgPass"></span>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="sexe" class="form-label">Sexe</label>
+                                <select class="form-select" id="sexe" name="sexe" aria-describedby="validationServer04Feedback">
+                                    <option selected disabled value="">Choisir</option>
+                                    <option value="F">Féminin</option>
+                                    <option value="M">Masculin</option>
+                                    <option value="A">Autres</option>
+                                </select>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="daten" class="form-label">Date de naissance</label>
+                                <input type="date" class="form-control is-valid" id="daten" name="daten">
+                            </div>
+                            <!-- <div class="col-md-12">
+                                <label for="photo" class="form-label">Photo</label>
+                                <input type="file" class="form-control is-valid" id="photo" name="photo">
+                            </div> -->
+                            <div class="col-12">
+                                <button class="btn btn-primary" type="submit">Enregistrer</button>
+                            </div>
+                        </form>
+                        </div>
+                        <div class="modal-footer">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Fin du modal pour enregistrer membre -->
+
+            <div class="modal fade" id="modalConnexion" tabindex="-1" aria-labelledby="ModalConnexionLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="ModalConnexionLabel">Connexion</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form class="row g-3" id="formConnexion" action="serveur/connexion/controleurConnexion.php" method="POST">
+                                <div class="col-md-4">
+                                    <label for="courriel" class="form-label">Courriel</label>
+                                    <input type="email" class="form-control" id="courrielco" name="courrielco" value="" required>
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="pass" class="form-label">Mot Passe</label>
+                                    <input type="password" class="form-control" pattern="[A-Za-z0-9_\$#\-]{6,10}$" id="mdpco" name="mdpco" required>
+                                </div>
+                                <input type="hidden" name="action" value="connexion">
+                                <div class="col-12">
+                                    <button class="btn btn-primary" type="submit">Connexion</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </div>
         </section>
 
