1382f380b3d93ff569319090c71e35f42e851398
FredericNguyen <fredericnguyen@yahoo.com>
2023-10-17 14:08:45 -0400

    created route controller

--- routes.php
+++ routes.php
@@ -1,10 +1,32 @@
 <?php
     // Au début de PHP: Déclarer les types dans les paramétres des fonctions
     declare (strict_types=1);
+    require_once(__DIR__."/serveur/Produit/ControleurProduit.php");
+    require_once(__DIR__."/serveur/Membre/ControleurMembre.php");
 
-    require_once(__DIR__."/serveur/Voyage/ControleurVoyage.php");
-   
-    $instanceCtr = ControleurVoyage::getControleurVoyage();
-    
-    echo $instanceCtr->CtrF_Actions();
+    function CtrP_Produit(){
+
+        $instanceCtr = ControleurProduit::getControleurProduit();
+        echo $instanceCtr->CtrP_Actions();
+    }
+
+    function CtrM_Membre(){
+
+        $instanceCtr = ControleurProduit::getControleurProduit();
+        echo $instanceCtr->CtrP_Actions();
+    }
+
+    function CtrR_Actions_Routes(){
+        $action=$_POST['type_req'];
+        switch($action){
+            case 'produit' :
+                return  CtrP_Produit();
+            case 'membre' :
+                return  CtrM_Membre();
+        }
+        // Retour de la réponse au client
+       
+    }
+
+    echo CtrR_Actions_Routes();
 ?>
